4ea13df7ea49b129e599e82c27ef28c1022c00f9
Shuodong Lou <chenfei_lou@sina.com>
2017-12-24 13:40:19 +0800

    Initialization of project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { AgentPage } from '../pages/agent/agent';
 
 @Component({
   templateUrl: 'app.html'
@@ -22,7 +23,8 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'List', component: ListPage },
+      { title: 'Agent', component: AgentPage }
     ];
 
   }
