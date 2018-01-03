553ef1227311fb17938ff75f3e8a942d72f93fc5
Shuodong Lou <chenfei_lou@sina.com>
2018-01-03 13:30:30 +0800

    for ionic

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
 /**
  * Generated class for the SignupPage page.
@@ -8,6 +9,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
  * Ionic pages and navigation.
  */
 
+
 @IonicPage()
 @Component({
   selector: 'page-signup',
@@ -15,7 +17,14 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SignupPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  authForm: FormGroup;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder) {
+    this.authForm = formBuilder.group({
+        phoneNum: ['', Validators.compose([Validators.required, Validators.pattern('[0-9]*'), Validators.minLength(11), Validators.maxLength(11)])],
+        password: ['', Validators.compose([Validators.required, Validators.minLength(8)])],
+        re_password: ['', Validators.compose([Validators.required, Validators.minLength(8)])]
+    });
   }
 
   ionViewDidLoad() {
